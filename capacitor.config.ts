0ba639b8bc0bea81808711d7d402ee78368b4c9e
Ashok Kumar Gangavarapu <ashoka6@gmail.com>
2026-02-01 16:57:26 -0800

    commit files to main
    commit files to main

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,31 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.wpclife.blueberry',
-  appName: 'Blueberry Planner',
-  webDir: 'dist/public',
-  server: {
-    androidScheme: 'https',
-    iosScheme: 'https'
-  },
-  plugins: {
-    PushNotifications: {
-      presentationOptions: ['badge', 'sound', 'alert']
-    },
-    LocalNotifications: {
-      smallIcon: 'ic_stat_icon',
-      iconColor: '#D2691E'
-    },
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: '#D2691E',
-      showSpinner: false
-    },
-    StatusBar: {
-      backgroundColor: '#D2691E',
-      style: 'LIGHT'
-    }
-  }
+  appName: 'BlueberryPlanner',
+  webDir: 'dist/public'
 };
 
 export default config;
