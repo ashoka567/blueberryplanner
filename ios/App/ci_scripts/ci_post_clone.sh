871bbdfee77d00da7ad527ffb47a57f3eac474b3
Ashok Kumar Gangavarapu <ashoka6@gmail.com>
2026-02-04 10:35:55 -0800

    added npm install
    added npm install

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -e
+echo "Installing Node.js dependencies..."
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+npm install
+echo "Building web app..."
+npm run build
+echo "Syncing Capacitor..."
+npx cap sync ios
 echo "Installing CocoaPods dependencies..."
 cd "$CI_PRIMARY_REPOSITORY_PATH/ios/App"
 pod install
